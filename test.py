bb1e21523ecf23392915127f5be0a9742c8c0b01
ywattenberg <wattenberg@mailbox.org>
2025-05-03 19:37:51 +0200

    use relative path for conf

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import logging
 logging.basicConfig(format="[%(asctime)s][%(levelname)s][%(name)s] - %(message)s", level=logging.DEBUG)
 
 if __name__ == "__main__":
-    with open("/home/wattenberg/Documents/Repositories/nextcloud_backup_script/config.toml", "rb") as f:
+    with open("./config.toml", "rb") as f:
         config = tomllib.load(f)
 
     create_backup(config)
